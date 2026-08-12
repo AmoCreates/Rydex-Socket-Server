d6e90e84cf2a14f05779cf1f997671938969f3b7
AmoCreates <AmoCreates@outlook.com>
2026-08-12 21:27:27 +0530

    Add status to driver location update event

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ io.on("connection", async (socket) => {
 
 	socket.on("driver-location-update", ({bookingId, status, latitude, longitude}) => {
 		io.to(`ride-${bookingId}`).emit("driver-location", {
-			latitude, longitude
+			latitude, longitude, status
 		})
 	})
 
